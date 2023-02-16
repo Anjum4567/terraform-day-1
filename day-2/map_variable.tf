7fb7cecb5a4822b6ca29159c260911de4a44412b
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-16 14:16:50 +0530

    Update map_variable.tf

--- day-2/map_variable.tf
+++ day-2/map_variable.tf
@@ -1,1 +1,19 @@
+#map variable
+variable "usersage" {
+    type = map
+    default = {
+    gaurav =20
+    saurav = 30
+    }
+}
+output  "userage"{
+    value = "my name is gauarv and age is  ${lookup(var.usersage ,"gaurav")}"
+  
+}
 
+
+o/p:
+Changes to Outputs:
+  + userage = "my name is gauarv and age is  20"
+
+You can apply this plan to s
