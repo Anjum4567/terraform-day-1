53509a452367057f6f2e63443c47979740a2eb15
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-16 14:27:20 +0530

    Create map_variable_dynamically.tf

---
+++ day-2/map_variable_dynamically.tf
@@ -0,0 +1,51 @@
+#How to use map variable Dynamically
+
+
+
+variable "usersage" {
+    type = map
+    default = {
+    gaurav =20
+    saurav = 30
+    }
+}
+
+variable "username" {
+    type = string
+  
+}
+output  "userage"{
+    value = "my name is ${var.username} and age is  ${lookup(var.usersage ,"${var.username}")}"
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Changes to Outputs:
+  + userage = "my name is gaurav and age is  20"
