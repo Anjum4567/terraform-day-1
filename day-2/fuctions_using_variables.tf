b6d48948bd8f94f304fe78f738818efbc427f564
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-16 12:16:06 +0530

    Update fuctions_using_variables.tf

--- day-2/fuctions_using_variables.tf
+++ day-2/fuctions_using_variables.tf
@@ -1,13 +1,23 @@
-#join functions
+#functions
 
 
 variable users {
     type = list
-    default =["gaurav", "saurav", "ankit"]
+    default =["GaUraV", "SAURAV", "aNKiT"]
   
 }
 
 output printfirst {
-    value ="first user is ${join(",",var.users)}"
+    value ="first user is ${join("------>",var.users)}"
   
+}
+
+
+output helloworlduppercase {
+    value = "${upper(var.users[0])}"
+}
+
+
+output helloworldlowercase {
+    value =${lower(var.users[2]}"
 }
