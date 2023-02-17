f9814b9cf0dbb2933ead31ab652ad2ec150af7a7
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-17 11:04:51 +0530

    Update credential-ways-.tf

--- day-2/credential-ways-.tf
+++ day-2/credential-ways-.tf
@@ -1,1 +1,8 @@
+#how to assign credentials to terraform (multiple ways to configure credentials)
+1.using directly provider details in main .tf
 
+provider "aws" {
+  region     = 
+  access_key = 
+  secret_key = 
+}
