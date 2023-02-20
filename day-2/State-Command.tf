e1082161dd29d5215a64513a85a145a9f33b8eb3
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-20 16:09:59 +0530

    Delete State-Command.tf

--- day-2/State-Command.tf
+++
@@ -1,4 +0,0 @@
-
-    These two commands comes under Terraform Inspecting State
-    terraform state list: This command is used to list resources within a Terraform state.
-    terraform state show: This command is used to show the attributes of a single resource in the Terraform state.
