0a2b5b02bed7259b9268656668a4ebadf00712b5
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-20 16:02:38 +0530

    Delete init.tf

--- day-2/init.tf
+++
@@ -1,5 +0,0 @@
-Initialization
-
-The first command to run for a new configuration — or after checking out an existing configuration from version control — is terraform init. Subsequent commands will use local settings and data that are initialized by terraform init.
-
-Terraform uses a plugin-based architecture to support hundreds of infrastructure and service providers. The terraform init command downloads and installs providers used within the configuration, which in this case is the aws provider.
