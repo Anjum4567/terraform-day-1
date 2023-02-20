256000abf00fca6d26e8a414f1b0561f5c5ad29a
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-20 12:13:29 +0530

    Create validate.tf

---
+++ day-2/validate.tf
@@ -0,0 +1,3 @@
+terraform validate
+
+The terraform validate command validates the configuration files in a directory.Validate runs checks that verify whether a configuration is syntactically valid and thus primarily useful for general verification of reusable modules, including the correctness of attribute names and value types.
