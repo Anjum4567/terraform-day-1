d55b18f80c8160ac311ae7a8a6b354470a3ff831
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-20 12:23:08 +0530

    Create show.tf

---
+++ day-2/show.tf
@@ -0,0 +1,3 @@
+terraform show
+
+The terraform show command is used to provide human-readable output from a state or plan file.This can be used to inspect a plan to ensure that the planned operations are expected, or to inspect the current state as Terraform sees it.
