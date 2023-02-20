f5aa704e7234e58b0d0aa62c36a98c32a1448891
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-20 16:07:38 +0530

    Delete refresh.tf

--- day-2/refresh.tf
+++
@@ -1,3 +0,0 @@
-terraform refresh
-
-The terraform refresh command reads the current settings from all managed remote objects and updates the Terraform state to match. This does not modify infrastructure but does modify the state file.
