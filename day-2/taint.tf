0dffa969cbad425178ac330b6d6aa8d9ff40702e
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-20 12:24:08 +0530

    Create taint.tf

---
+++ day-2/taint.tf
@@ -0,0 +1,3 @@
+terraform taint
+
+The terraform taint command informs Terraform that a particular object has become degraded or damaged. Terraform represents this by marking the object as “tainted” in the Terraform state, and Terraform will propose to replace it in the next plan you create.
