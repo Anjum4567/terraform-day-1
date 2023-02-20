65c967960152b9f4f5a9886fb5131cd52d9bad95
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-20 12:20:48 +0530

    Create graph.tf

---
+++ day-2/graph.tf
@@ -0,0 +1,5 @@
+terraform graph
+
+The terraform graph command is used to generate a visual representation of either a configuration or execution plan. The output is in the DOT format, which can be used by GraphViz to generate charts.
+
+
