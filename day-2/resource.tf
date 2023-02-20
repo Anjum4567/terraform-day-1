f961baea605691ead5359be84fa8528e5f13e948
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-20 15:56:51 +0530

    Delete resource.tf

--- day-2/resource.tf
+++
@@ -1,5 +0,0 @@
-Resources
-
-The resource block defines a piece of infrastructure. A resource might be a physical component such as an EC2 instance, or it can be a logical resource such as a Heroku application.
-
-The resource block has two strings before the block: the resource type and the resource name. In the example, the resource type is “aws_instance” and the name is “example.” The prefix of the type maps to the provider. In our case “aws_instance” automatically tells Terraform that it is managed by the “aws” provider
