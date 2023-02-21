93e7e517c82bbcce9b3958f50dc6a7858ff49c0f
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-22 00:19:59 +0530

    Update provider.tf

--- day 3/modules/webserver/provider.tf
+++ day 3/modules/webserver/provider.tf
@@ -1,7 +1,7 @@
 #ec2 instances creation
 resource "aws_instance" "ec2-machine-1" {
 
-  ami           = "ami-0f8ca728008ff5af4"
-  instance_type = "t2.micro"
+  ami           = var.image-id
+  instance_type = var.instance-type
 
 }
