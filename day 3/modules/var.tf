8a76825d2eac65dd99f129ed83cb424e63af18a7
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-22 11:40:39 +0530

    Create var.tf

---
+++ day 3/modules/var.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+  type        = string
+  default     = "
+  description = "for aws account 1"
+
+
+}
+variable "secret_key" {
+  type        = string
+  default     = "
+  description = "for aws secret key "
+
+}
+variable "web_region" {
+  type        = string
+  default     = 
+  description = "for aws region "
+
+}
