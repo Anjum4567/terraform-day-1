23fc173dae5f0ff4b857d911a43299bc8a7a914b
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-24 19:03:23 +0530

    Create var.tf

---
+++ day 3/Q-3/var.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  type        = string
+  default     = "
+  description = "for aws account 1"
+}
+variable "secret_key" {
+  type        = string
+  default     = "
+  description = "for aws secret key "
+
+}
