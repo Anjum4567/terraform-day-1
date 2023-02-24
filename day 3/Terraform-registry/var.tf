8529ce66cd40015583b6ba4e6815d922cd91f012
Anjum4567 <102940925+Anjum4567@users.noreply.github.com>
2023-02-24 15:17:29 +0530

    Create var.tf

---
+++ day 3/Terraform-registry/var.tf
@@ -0,0 +1,13 @@
+variable "access_key" {
+  type        = string
+  default     = "AKIAQGCHQED4WUGBSSGF"
+  description = "for aws account 1"
+
+
+}
+variable "secret_key" {
+  type        = string
+  default     = "iXThCcDYsGkp4+e0+Rn8AVI/aAyGjNGYG7GlmAuN"
+  description = "for aws secret key "
+
+}
